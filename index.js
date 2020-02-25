95a8fd6bdbdcc423fd9f95b3cd1f6c45469e04fd
Juan Pablo Reyes Gomez <jp.reyes39@uniandes.edu.co>
2020-02-25 15:57:06 -0500

    adding post

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ module.exports = app => {
 
   })
 	
-	app.post('/access', function (req, res, next))
+	app.router.post('/access', function (req, res, next)
 	{
 		app.log("Hello")
-	}
+	})
 
   
 }
