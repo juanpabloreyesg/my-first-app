ae2f6213a01086e8579ad646eff3cfe5dd361b88
Juan Pablo Reyes Gomez <jp.reyes39@uniandes.edu.co>
2020-02-27 13:20:06 -0500

    correcting again

--- lti.js
+++ lti.js
@@ -1,6 +1,6 @@
-var config = require("./config");
-var consumer_key = config.lti.key;
-var consumer_secret = config.lti.secret;
+var config = require("./config/confit-lti");
+var consumer_key = config.development.key;
+var consumer_secret = config.development.secret;
 var lti = require('ims-lti');
 var Promise = require('bluebird');
 //var db = require("../../models");
@@ -11,31 +11,38 @@ var Promise = require('bluebird');
  * Genera un registro del usuario, para su posterior calificación.
  */
 function registrarIngreso(req) {
-    return new Promise(function (resolve, reject) {
-        var provider = new lti.Provider(consumer_key, consumer_secret);
-        provider.valid_request(req, function (err, is_valid) {
-            var body = req.body;
-            if (!is_valid || !provider.outcome_service) return reject(new Error("El envío de los parámetros desde Coursera no coincide."));
-            if (!body.custom_examen) return reject(new Error('Es necesario indicar el id del examen en los parámetros de personalización de la actividad. Por ejemplo, llave: examen y valor: 1'));
+  return new Promise(function(resolve, reject) {
+    var provider = new lti.Provider(consumer_key, consumer_secret);
+    provider.valid_request(req, function(err, is_valid) {
+      var body = req.body;
+      if (!is_valid || !provider.outcome_service)
+        return reject(
+          new Error("El envío de los parámetros desde Coursera no coincide.")
+        );
+      if (!body.custom_examen)
+        return reject(
+          new Error(
+            "Es necesario indicar el id del examen en los parámetros de personalización de la actividad. Por ejemplo, llave: examen y valor: 1"
+          )
+        );
 
-            var actividad = body.resource_link_title;
-            var nombre = body.lis_person_name_full;
-            var userId = body.user_id;
-            var examenId = body.custom_examen;
-            var serviceUrl = body.lis_outcome_service_url;
-            var sourcedId = body.lis_result_sourcedid;
-            var respuestaExamen = {
-                    ExamenId: examenId,
-                    EstudianteId: userId,
-                    lis_outcome_service_url: serviceUrl,
-                    lis_result_sourcedid: sourcedId,
-                    actividad: actividad
-                };
+      var actividad = body.resource_link_title;
+      var nombre = body.lis_person_name_full;
+      var userId = body.user_id;
+      var examenId = body.custom_examen;
+      var serviceUrl = body.lis_outcome_service_url;
+      var sourcedId = body.lis_result_sourcedid;
+      var respuestaExamen = {
+        ExamenId: examenId,
+        EstudianteId: userId,
+        lis_outcome_service_url: serviceUrl,
+        lis_result_sourcedid: sourcedId,
+        actividad: actividad
+      };
 
-            return respuestaExamen;
-            });
-        
-	)};
+      return respuestaExamen;
+    });
+  });
 }
 
 
